0a58ef456535150da671b2ae3db7f3910f18b9ca
Kamil Frydlewicz <kamil@frydlewicz.pl>
2023-05-17 17:36:21 +0200

    pug package

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { resolve } from 'path';
+
 import { AppModule } from './app.module';
 
+const port = parseInt(process.env.PORT || '3000');
+
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+    app.setViewEngine('pug');
+    app.setBaseViewsDir(resolve('./views/pages'));
+    app.disable('x-powered-by');
+    await app.listen(port);
+
+    console.info(`Listening on port ${port}.`);
 }
 bootstrap();
