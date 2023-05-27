13a723a4b3ef3d57321999846cc0463de199e6d1
Kamil Frydlewicz <kamil@frydlewicz.pl>
2023-05-27 02:13:48 +0200

    board orientation

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,9 +1,10 @@
 import { io } from '/scripts/socket.io.esm.min.js';
-import { Chessboard } from 'https://cdn.jsdelivr.net/npm/cm-chessboard@7/src/Chessboard.min.js';
+import { Chessboard, BORDER_TYPE, COLOR } from 'https://cdn.jsdelivr.net/npm/cm-chessboard@7/src/Chessboard.min.js';
 import { FEN } from 'https://cdn.jsdelivr.net/npm/cm-chessboard@7/src/model/Position.min.js';
 
 const socket = io();
 let board;
+let loadedStyle = false;
 
 export function createGame(ai) {
     socket.emit('create', { ai }, (res) => {
@@ -25,9 +26,9 @@ export function joinGame(host, ai, roomId) {
     sessionStorage.setItem('name', name);
 
     socket.emit('join', { host, ai, roomId, name }, (res) => {
-        if (res?.status === 'success') {
+        if (res?.status === 'success' && res.state) {
             loadStyle().then(() => {
-                buildBoard();
+                buildBoard(host, res.state);
             });
         } else if (res?.status === 'error' && res.display) {
             alert(res.display);
@@ -38,10 +39,17 @@ export function joinGame(host, ai, roomId) {
 }
 
 function loadStyle() {
+    if (loadedStyle) {
+        return Promise.resolve();
+    }
+
     return new Promise((res) => {
         const link = document.createElement('link');
 
-        link.onload = () => res();
+        link.onload = () => {
+            loadedStyle = true;
+            res();
+        };
         link.rel = 'stylesheet';
         link.href = 'https://cdn.jsdelivr.net/npm/cm-chessboard@7/assets/chessboard.min.css';
 
@@ -49,11 +57,16 @@ function loadStyle() {
     });
 }
 
-function buildBoard() {
+function buildBoard(host, state) {
     board = new Chessboard(document.getElementById('board'), {
+        orientation: host && state.host.side === 0 || !host && state.guest.side === 0
+            ? COLOR.white
+            : COLOR.black,
         position: FEN.start,
         assetsUrl: 'https://cdn.jsdelivr.net/npm/cm-chessboard@7/assets/',
         style: {
+            cssClass: 'chess-club',
+            borderType: BORDER_TYPE.frame,
             pieces: {
                 file: 'staunty.svg',
             },
